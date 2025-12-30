e6268fd16d70d39cb2a79811e4f36546016fe968
Salimrash <salimrash91@gmail.com>
2025-12-30 17:05:47 +0530

    NavBar,Banner added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import NavBar from "./components/navbar/NavBar";
+import './App.css'
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div>
-      
+      <NavBar/>
+      <Banner/>
     </div>
   );
 }
