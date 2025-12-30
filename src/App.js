97a159e9bbee9aa49348c20026c0224a58f8b8bd
Salimrash <salimrash91@gmail.com>
2025-12-31 01:05:26 +0530

    RowPost added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/navbar/NavBar";
 import './App.css'
 import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
+      <RowPost/>
     </div>
   );
 }
