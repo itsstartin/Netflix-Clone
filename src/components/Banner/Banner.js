13033ff6f51cbc2db3964388f144fa66b469e4ce
Salimrash <salimrash91@gmail.com>
2026-01-06 03:27:31 +0530

    Added axios(to get data from website using API),used tmdb api and got movie name to the banner name from trending

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import { API_KEY } from '../../constants/constants'
 function Banner() {
+  const [banner,setBanner]=useState()
+  useEffect(()=>{
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce)=>{
+      console.log(responce.data.results[0])
+      setBanner(responce.data.results[0])
+
+
+    })
+  },[])
   return (
     <div className='banner'>
         <div className="content">
-            <h1 className='title'>MovieName</h1>
+            <h1 className='title'>{banner ? banner.name :""}</h1>
             <div className="banner_buttons">
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const baseURL="https://api.themoviedb.org/3"
+export const API_KEY="1ac5bdb5262ec694f6300cfb9ea56492"
